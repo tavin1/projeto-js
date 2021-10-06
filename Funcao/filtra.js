fc19b330629e11e8dd231d8301be92d07b2e36f8
Otavio Fernandes <otaviospa@hotmail.com>
2021-10-06 14:30:03 -0300

    atualizando minhas atividades de JS

---
+++ Funcao/filtra.js
@@ -0,0 +1,16 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function(){
+    var pacientes = document.querySelectorAll(".paciente");
+        for(var i = 0;  i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome   = paciente.querySelector(".info-nome"); 
+            var nome     = tdNome.textContent;
+            if (nome != this.value){
+                paciente.classList.add("invisivel");
+            }
+            else{
+                
+            }
+        }
+});
